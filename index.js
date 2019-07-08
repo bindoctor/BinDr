b27ed390531a318e84482b50a6a862d2f71dc73d
Geoff Smith <smith260194@gmail.com>
2019-07-08 16:42:45 +0100

    Adding handlebars to the project | telling express about handlebars | adding some basic views

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
 const app = express()
 
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+
 app.get('/', (request, response) => {
-  response.send('Hello');
+  response.render('home');
+})
+
+app.get('/about', (request, response) => {
+  response.render('about');
 })
 
 const port = 3000
