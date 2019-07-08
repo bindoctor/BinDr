007d3cf10a7e45d3546f508446ec650615fa4608
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-07-08 15:19:30 +0100

    set up node, express, eslint

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send("Hello")
+})
+
+app.listen(3000, () => console.log(`Your app is listening on port 3000`))
+
