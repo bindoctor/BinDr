fbab78a802a349c03c38dc604ca32aca2059b1fb
Anna <annas.wieclaw@gmail.com>
2019-07-08 16:48:43 +0100

    Add Travis file

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,10 +7,6 @@ const api = request(app);
 
 describe('main page loads', () => {
 
-  // afterEach(async () => {
-  //   await server.close();
-  // })
-
   test('shows main page', () => {
     return api.get('/').expect(200);
   });
