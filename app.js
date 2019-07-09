9e6bf600042b2f7fc93dbcf060dd10807e4bc685
Geoff Smith <smith260194@gmail.com>
2019-07-09 13:45:12 +0100

    Adding database connection

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-const express = require('express');
-const expressHandlebars = require('express-handlebars');
-const app = express();
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const app = express()
 const sampleData = require('./sampleData')
+const bin = require('./models/bin')
 
-app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 
 app.get('/', (request, response) => {
-  response.render('home');
-});
+  response.render('home')
+})
 
 app.get('/about', (request, response) => {
-  response.render('about');
-});
+  response.render('about')
+})
 
 app.get('/api/sample-bins', (request, response) => {
-  response.json(sampleData)
-});
+  const binJson = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'Point',
+      coordinates: [
+        -0.0996708869934082,
+        51.52560021903987,
+      ],
+    },
+  }
 
-module.exports = app;
+  const newBin = new bin(binJson)
+  newBin.save()
+  response.json(sampleData)
+})
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // we split index.js into app.js and server.js so
 // the tests can run without the server running
+require('dotenv').config();
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI)
 
 app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}`));
 
