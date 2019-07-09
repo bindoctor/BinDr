4e9a8947a949bca78c3c361e1aece300dc30d024
Anna <annas.wieclaw@gmail.com>
2019-07-09 09:26:42 +0100

    Fix the open hanlde issue with wait time of 500ms we think

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,6 +3,12 @@ const app = require('../app');
 const request = require('supertest');
 const api = request(app);
 
+afterAll(async () => {
+  await new Promise((resolve) => setTimeout(() => {
+    resolve();
+  }, 500)); // avoid jest open handle error
+});
+
 describe('main page loads', () => {
   test('shows main page', () => {
     return api.get('/').expect(200);
@@ -11,6 +17,6 @@ describe('main page loads', () => {
 
 describe('about page loads', () => {
   test('shows about page', () => {
-    api.get('/about').expect(200);
+    return api.get('/about').expect(200);
   });
 });
