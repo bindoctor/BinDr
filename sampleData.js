8fd246bb8f8d27e9d18991174ee33034334b795e
Helen Hayward <helen.hayward@ft.com>
2019-07-09 11:26:34 +0100

    Test api/sample-bins

--- sampleData.js
+++ sampleData.js
@@ -1,49 +1,49 @@
 module.exports = {
-  "type": "FeatureCollection",
-  "features": [
+  'type': 'FeatureCollection',
+  'features': [
     {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [
           -0.0996708869934082,
-          51.52560021903987
-        ]
-      }
+          51.52560021903987,
+        ],
+      },
     },
     {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [
           -0.09953141212463379,
-          51.52519971096638
-        ]
-      }
+          51.52519971096638,
+        ],
+      },
     },
     {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [
           -0.09926319122314453,
-          51.52453218968203
-        ]
-      }
+          51.52453218968203,
+        ],
+      },
     },
     {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [
           -0.09997129440307617,
-          51.52614757437653
-        ]
-      }
-    }
-  ]
-}
+          51.52614757437653,
+        ],
+      },
+    },
+  ],
+};
