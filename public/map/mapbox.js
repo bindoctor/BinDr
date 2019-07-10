9f4264ec0400e0eddc0784a950f13ad126f3d773
Anthony Hull <m@anthonyhull.me>
2019-07-10 22:02:47 +0100

    replaced map with mapbox and added recycling icons

---
+++ public/map/mapbox.js
@@ -0,0 +1,29 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW50aG9ueWh1bGwiLCJhIjoiY2p1Y2ZzZ2YxMDF5YTQ0b3pnM24wZHo2MiJ9.hMM0VZCqVFUmgqPjh1dqfg';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-0.099207, 51.524531],
+zoom: 15
+});
+
+
+
+map.on('load', function() {
+map.addSource('allBins', {
+    type: 'geojson',
+    data: '/api/bins'
+});
+map.loadImage('/images/kissclipart-recycle-png-clipart-recycling-symbol-recycling-bin-d54071ced034dc09.png', function(error, image) {
+    if (error) throw error;
+    map.addImage('recyclingBin', image);
+    map.addLayer({
+        "id": "points",
+        "type": "symbol",
+        "source": "allBins",
+        "layout": {
+            "icon-image": "recyclingBin",
+            "icon-size": 0.1
+            }
+        });
+    });
+});
