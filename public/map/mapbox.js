f6358b366eddb86253eaaaacbfc8b8b3f0ccb7c5
Helen Hayward <helen.hayward@ft.com>
2019-07-11 14:57:49 +0100

    map styling

--- public/map/mapbox.js
+++ public/map/mapbox.js
@@ -1,11 +1,11 @@
 // mapboxgl.accessToken = 'pk.eyJ1IjoiYW50aG9ueWh1bGwiLCJhIjoiY2p4eWlub2k2MDV4YjNxcWpvaXk3YjRhNyJ9.XK5kjKyu40OBZGX4Bom0oA';
 var map = new mapboxgl.Map({
 container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11',
+// style: 'mapbox://styles/mapbox/streets-v11',
+style: 'mapbox://styles/bindr/cjxyplq2c064h1cmxgpyefs28',
 center: [-0.099207, 51.524531],
 zoom: 15
 });
-
 
 
 map.on('load', function() {
@@ -17,6 +17,7 @@ const geoLocation = new mapboxgl.GeolocateControl({
     showUserLocation: true
 });
 map.addControl(geoLocation);
+
 
 
 map.addSource('allBins', {
