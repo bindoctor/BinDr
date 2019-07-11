b7052ff972561df6eea512997d8ee219628759b3
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-07-11 12:39:58 +0100

    add bin type model, create script to insert mixed bin type

--- scripts/insertData.js
+++ scripts/insertData.js
@@ -5,7 +5,9 @@ const fs = require('fs');
 const contents = fs.readFileSync('../tests/testData.json');
 const jsonContent = JSON.parse(contents);
 const Bin = require('../models/bin');
+const BinType = require('../models/binType');
 
+console.log (process.env.MONGODB_DEVURI)
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 Bin.insertMany(jsonContent, () => mongoose.disconnect() );
