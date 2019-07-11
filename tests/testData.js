6dd4b8a5f7b0b2bb9be992ff5227cb5a68f14194
Anna <annas.wieclaw@gmail.com>
2019-07-11 15:37:04 +0100

    Set up test bin types and insterted then into test bin data

--- tests/testData.js
+++ tests/testData.js
@@ -30,8 +30,8 @@ const plastic = new BinType({
     iconSize: 0.3
   }
 });
-
-[
+exports.binTypes = [plastic, glass, mixed, paper];
+exports.bins = [
   {
     type: 'Feature',
     properties: plastic._id,
@@ -39,217 +39,218 @@ const plastic = new BinType({
       type: 'Point',
       coordinates: [
         -0.09943485260009766,
-        51.524992780414806
-      ]
-    }
+        51.524992780414806,
+      ],
+    },
   },
   {
     type: 'Feature',
-    properties: {},
+    properties: mixed._id,
     geometry: {
       type: 'Point',
       coordinates: [
         -0.09907007217407227,
-        51.524191750059074
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.1001429557800293,
-        51.52618094948909
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.09752511978149414,
-        51.52690184594944
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.10048627853393555,
-        51.52755598286007
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: plastic._id,
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.09628057479858398,
-        51.52525978740199
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.1028895378112793,
-        51.52594064813257
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.10402679443359375,
-        51.526020748725664
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.10340452194213866,
-        51.524445411196304
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.10192394256591797,
-        51.52248283820342
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.09735345840454102,
-        51.52150819569328
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.09525060653686523,
-        51.52341740732051
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.09894132614135742,
-        51.527782926123
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.10271787643432617,
-        51.52724894017439
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.09256839752197266,
-        51.52644794950893
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.09334087371826172,
-        51.524805874592204
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.09282588958740234,
-        51.52489932759941
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.10391950607299805,
-        51.522522891558964
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.09460687637329102,
-        51.527369087558846
-      ]
-    }
-  },
-  {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [
-        -0.09709596633911133,
-        51.52838365282935
-      ]
-    }
+        51.524191750059074,
+      ],
+    },
   }
-]
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.1001429557800293,
+  //       51.52618094948909,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.09752511978149414,
+  //       51.52690184594944,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.10048627853393555,
+  //       51.52755598286007,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: plastic._id,
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.09628057479858398,
+  //       51.52525978740199,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.1028895378112793,
+  //       51.52594064813257,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.10402679443359375,
+  //       51.526020748725664,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.10340452194213866,
+  //       51.524445411196304,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.10192394256591797,
+  //       51.52248283820342,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.09735345840454102,
+  //       51.52150819569328,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.09525060653686523,
+  //       51.52341740732051,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.09894132614135742,
+  //       51.527782926123,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.10271787643432617,
+  //       51.52724894017439,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.09256839752197266,
+  //       51.52644794950893,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.09334087371826172,
+  //       51.524805874592204,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.09282588958740234,
+  //       51.52489932759941,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.10391950607299805,
+  //       51.522522891558964,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.09460687637329102,
+  //       51.527369087558846,
+  //     ],
+  //   },
+  // },
+  // {
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [
+  //       -0.09709596633911133,
+  //       51.52838365282935,
+  //     ],
+  //   },
+  // },
+];
+
