c732b529117b461125d597849bab56586c7b7a2d
Anna <annas.wieclaw@gmail.com>
2019-07-11 11:12:18 +0100

    Tracking with GoogleMaps works

--- public/map/gmaps.js
+++ public/map/gmaps.js
@@ -2,149 +2,155 @@
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
-var map, infoWindow;
-var im = 'http://www.robotwoods.com/dev/misc/bluecircle.png';
+
+let map; let infoWindow;
+const im = 'http://www.robotwoods.com/dev/misc/bluecircle.png';
+// let latitude;
+// let longitude;
+let ourBouncingBallMarker;
+let lng; let lat;
+
 
 function initMap() {
-  var styledMapType = new google.maps.StyledMapType(
-    [
-      {
-          "featureType": "all",
-          "elementType": "labels.text.fill",
-          "stylers": [
-              {
-                  "color": "#ffffff"
-              }
-          ]
-      },
-      {
-          "featureType": "all",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 13
-              }
-          ]
-      },
-      {
-          "featureType": "administrative",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#000000"
-              }
-          ]
-      },
-      {
-          "featureType": "administrative",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "color": "#144b53"
-              },
-              {
-                  "lightness": 14
-              },
-              {
-                  "weight": 1.4
-              }
-          ]
-      },
-      {
-          "featureType": "landscape",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "color": "#08304b"
-              }
-          ]
-      },
-      {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#0c4152"
-              },
-              {
-                  "lightness": 5
-              }
-          ]
-      },
-      {
-          "featureType": "road.highway",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#000000"
-              }
-          ]
-      },
-      {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "color": "#0b434f"
-              },
-              {
-                  "lightness": 25
-              }
-          ]
-      },
-      {
-          "featureType": "road.arterial",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#000000"
-              }
-          ]
-      },
-      {
-          "featureType": "road.arterial",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "color": "#0b3d51"
-              },
-              {
-                  "lightness": 16
-              }
-          ]
-      },
-      {
-          "featureType": "road.local",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#000000"
-              }
-          ]
-      },
-      {
-          "featureType": "transit",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "color": "#146474"
-              }
-          ]
-      },
-      {
-          "featureType": "water",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "color": "#021019"
-              }
-          ]
-      }
-  ],
-  {name: 'Styled Map'});
+  const styledMapType = new google.maps.StyledMapType(
+      [
+        {
+          'featureType': 'all',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#ffffff',
+            },
+          ],
+        },
+        {
+          'featureType': 'all',
+          'elementType': 'labels.text.stroke',
+          'stylers': [
+            {
+              'color': '#000000',
+            },
+            {
+              'lightness': 13,
+            },
+          ],
+        },
+        {
+          'featureType': 'administrative',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'color': '#000000',
+            },
+          ],
+        },
+        {
+          'featureType': 'administrative',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#144b53',
+            },
+            {
+              'lightness': 14,
+            },
+            {
+              'weight': 1.4,
+            },
+          ],
+        },
+        {
+          'featureType': 'landscape',
+          'elementType': 'all',
+          'stylers': [
+            {
+              'color': '#08304b',
+            },
+          ],
+        },
+        {
+          'featureType': 'poi',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#0c4152',
+            },
+            {
+              'lightness': 5,
+            },
+          ],
+        },
+        {
+          'featureType': 'road.highway',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'color': '#000000',
+            },
+          ],
+        },
+        {
+          'featureType': 'road.highway',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#0b434f',
+            },
+            {
+              'lightness': 25,
+            },
+          ],
+        },
+        {
+          'featureType': 'road.arterial',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'color': '#000000',
+            },
+          ],
+        },
+        {
+          'featureType': 'road.arterial',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#0b3d51',
+            },
+            {
+              'lightness': 16,
+            },
+          ],
+        },
+        {
+          'featureType': 'road.local',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000',
+            },
+          ],
+        },
+        {
+          'featureType': 'transit',
+          'elementType': 'all',
+          'stylers': [
+            {
+              'color': '#146474',
+            },
+          ],
+        },
+        {
+          'featureType': 'water',
+          'elementType': 'all',
+          'stylers': [
+            {
+              'color': '#021019',
+            },
+          ],
+        },
+      ],
+      {name: 'Styled Map'});
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 51.508, lng: -0.075},
@@ -153,7 +159,7 @@ function initMap() {
     disableDefaultUI: true,
   });
 
-  //Associate the styled map with the MapTypeId and set it to display.
+  // Associate the styled map with the MapTypeId and set it to display.
   map.mapTypes.set('styled_map', styledMapType);
   map.setMapTypeId('styled_map');
 
@@ -162,30 +168,53 @@ function initMap() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       updatePosition(position);
-      map.setCenter(new google.maps.LatLng(latitude, longitude));
+      map.setCenter(new google.maps.LatLng(lat, lng));
 
-      const ourBouncingBallMarker = new google.maps.Marker({
+      ourBouncingBallMarker = new google.maps.Marker({
         position: currentLocation(),
         map: map,
         icon: im,
         title: 'Hello I live here!',
-        animation: google.maps.Animation.BOUNCE
+        animation: google.maps.Animation.BOUNCE,
       });
 
-    }, function () {
-      handleLocationError(true, infoWindow, map.getCenter());
     });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
   }
-  map.data.loadGeoJson('/api/sample-bins');
+  //   }, function() {
+  //     handleLocationError(true, infoWindow, map.getCenter());
+  //   });
+  // } else {
+  //   // Browser doesn't support Geolocation
+  //   handleLocationError(false, infoWindow, map.getCenter());
+  // }
+  // map.data.loadGeoJson('/api/sample-bins');
+  //
+  // trackLocation({
+  //   onSuccess: ({coords: {latitude: latitude, longtitude: longitude}}) => {
+  //     ourBouncingBallMarker.setPosition({lat: latitude, lng: longitude});
+  //     map.panTo({lat: latitude, lng: longitude});
+  //   },
+  //   onError: (err) =>
+  //     alert(`Error: ${getPositionErrorMessage(err.code) || err.message}`),
+  // });
+
+  let watchId = trackLocation({
+    onSuccess: ({ coords: { latitude: lat, longitude: lng } }) => {
+      ourBouncingBallMarker.setPosition({ lat, lng });
+      map.panTo({ lat, lng });
+    },
+    onError: err => {
+      console.log($info);
+    }
+  });
+
+
 }
 
 function updatePosition(position) {
   if (position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
   }
 }
 
@@ -195,12 +224,12 @@ function getLocation() {
   }
 }
 
-setInterval(function() {
-  updatePosition(getLocation());
-}, 10000);
+// setInterval(function() {
+//   updatePosition(getLocation());
+// }, 10000);
 
 function currentLocation() {
-  return {lat: latitude, lng: longitude};
+  return {lat: lat, lng: lng};
 };
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -210,6 +239,17 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
 }
+
+const trackLocation = ({onSuccess, onError = () => { }}) => {
+  if ('geolocation' in navigator === false) {
+    return onError(new Error('Geolocation is not supported by your browser.'));
+  }
+  return navigator.geolocation.watchPosition(onSuccess, onError, {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  });
+};
 
 // var redraw = function(payload) {
 //   updatePosition(payload);
