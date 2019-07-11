4c91f71b2ab19f29529bffc60e70bd9182c979cc
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-07-11 15:57:11 +0100

    update in memory DB tests for bin types, update and run insertdata script

--- scripts/insertData.js
+++ scripts/insertData.js
@@ -9,11 +9,16 @@ const BinType = require('../models/binType');
 
 const data = require('../tests/testData.js');
 
-mongoose.connect('mongodb+srv://bindr:welovebins@cluster0-3lj98.mongodb.net/dev?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect(process.env.SCRIPT_DB_URL, {useNewUrlParser: true});
 
-BinType.insertMany(data.binTypes, () => {
-  Bin.insertMany(data.bins, () => mongoose.disconnect());
+Bin.deleteMany({}, () => {
+  BinType.deleteMany({}, () => {
+    BinType.insertMany(data.binTypes, () => {
+      Bin.insertMany(data.bins, () => mongoose.disconnect());
+    });
+  });
 });
+
 
 // BinType.findOne({binTypeName: 'Mixed'}, function(err, result) {
 //   const aBin = {
