fe499194b29db6057078823539f56ac9148faa62
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-07-12 09:34:43 +0100

    add recommended config var for Heroku to applyEnforceHttps.js

--- applyEnforceHttps.js
+++ applyEnforceHttps.js
@@ -2,7 +2,7 @@ const enforce = require('express-sslify');
 
 const applyEnforceHttps = (app) => {
   if (app.get('env') !== 'test' && app.get('env') !== 'dev' ) {
-    app.use(enforce.HTTPS());
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
   }
   ;
 };
