02d88bc9c47889d297e819b5cd2e484fa63745c0
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-07-12 09:32:24 +0100

    tidy up - remove extra spaces added to server.js, remove un-needed addition to app.test.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 // we split index.js into app.js and server.js so
 // the tests can run without the server running
 require('dotenv').config();
-
 const app = require('./app');
-
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 
--- tests/app.test.js
+++ tests/app.test.js
@@ -3,7 +3,6 @@ const GJV = require('geojson-validation');
 const request = require('supertest');
 const api = request(app);
 require('./inMemoryDatabaseTestHelper');
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 
 describe('main page loads', () => {
