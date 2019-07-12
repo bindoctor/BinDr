e6d342c27ccf4bec26d139e6da7f34680a36e8a9
Anna <annas.wieclaw@gmail.com>
2019-07-12 15:21:01 +0100

    Add tests to satisfy travis with testing branches of the code

--- tests/users.test.js
+++ tests/users.test.js
@@ -8,57 +8,106 @@ let token;
 
 beforeEach(async () => {
   await api.post('/api/users')
-  .send({user: {
-    email: "user@test.com",
-    password: "password123"
-  }})
-  .set('Accept', 'application/json')
-  .then(response => {
-    token = response.body.user.token;
-  })
+      .send({user: {
+        email: 'user@test.com',
+        password: 'password123',
+      }})
+      .set('Accept', 'application/json')
+      .then((response) => {
+        token = response.body.user.token;
+      });
 });
 
 
 describe('/api/users', () => {
   test('accepts user credentials', () => {
     return api.post('/api/users')
+        .send({user: {
+          email: 'user@test.com',
+          password: 'password123',
+        }})
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200);
+  });
+  test('reports 422 status code when no email specified', () => {
+    return api.post('/api/users')
+        .send({user: {
+          email: '',
+          password: 'password123',
+        }})
+        .set('Accept', 'application/json')
+        .expect(422)
+        .then((response) => {
+          expect(response.body.errors.email).toEqual('is required');
+        });
+  });
+  test('reports 422 status code when no password specified', () => {
+    return api.post('/api/users')
       .send({user: {
-        email: "user@test.com",
-        password: "password123"
-      }})
+          email: 'user@test.com',
+          password: '',
+        }})
       .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200);
+      .expect(422)
+      .then((response) => {
+        expect(response.body.errors.password).toEqual('is required');
+      });
   });
 });
+
 
 describe('/api/users/current', () => {
   test('when logged in I can see stuff', async () => {
     return api.get('/api/users/current')
-      .set('Authorization', 'Token ' + token)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then(response => {
-        expect(response.body.user.email).toEqual('user@test.com')
-        expect(response.body.user.token).toEqual(token)
-      })
+        .set('Authorization', 'Token ' + token)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body.user.email).toEqual('user@test.com');
+          expect(response.body.user.token).toEqual(token);
+        });
   });
 });
 
 describe('/api/users/login', () => {
-  test ('can login', async () => {
+  test('can login', async () => {
     return api.post('/api/users/login')
-      .set('Accept', 'application/json')
+        .set('Accept', 'application/json')
+        .send({user: {
+          email: 'user@test.com',
+          password: 'password123',
+        }})
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body.user.email).toEqual('user@test.com');
+          expect(typeof(response.body.user.token)).toEqual('string');
+        });
+  });
+  test('reports 422 status code when no email specified', () => {
+    return api.post('/api/users/login')
       .send({user: {
-        email: "user@test.com",
-        password: "password123"
-      }})
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then(response => {
-        expect(response.body.user.email).toEqual('user@test.com')
-        expect(typeof(response.body.user.token)).toEqual('string')
-      })
-  })
-})
+          email: '',
+          password: 'password123',
+        }})
+      .set('Accept', 'application/json')
+      .expect(422)
+      .then((response) => {
+        expect(response.body.errors.email).toEqual('is required');
+      });
+  });
+  test('reports 422 status code when no password specified', () => {
+    return api.post('/api/users/login')
+      .send({user: {
+          email: 'user@test.com',
+          password: '',
+        }})
+      .set('Accept', 'application/json')
+      .expect(422)
+      .then((response) => {
+        expect(response.body.errors.password).toEqual('is required');
+      });
+  });
+});
