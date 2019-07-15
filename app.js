2352f09f2a0e795927bf9a7576988b37103dbbcd
Helen Hayward <helen.hayward@ft.com>
2019-07-15 22:05:11 +0100

    req.bodt identified. Need to add to database

--- app.js
+++ app.js
@@ -1,54 +1,70 @@
-const express = require('express');
-const expressHandlebars = require('express-handlebars');
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
 const app = express();
-const sampleData = require('./sampleData');
-const bodyParser = require('body-parser');
-require('./models/user');
-require('./models/bin');
-require('./config/passport');
+const sampleData = require("./sampleData");
+const bodyParser = require("body-parser");
+require("./models/user");
+require("./models/bin");
+require("./config/passport");
 
-//The order of these statements is important
-app.use(express.static('public'));
+// The order of these statements is important
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-const applyMorganMiddleware = require('./middleware/morganMiddleware');
-const applyEnforceHttps = require('./config/applyEnforceHttps')
-require('dotenv').config();
+const applyMorganMiddleware = require("./middleware/morganMiddleware");
+const applyEnforceHttps = require("./config/applyEnforceHttps");
+require("dotenv").config();
 
-app.engine('handlebars', expressHandlebars({
-  defaultLayout: 'main',
-  helpers: {
-    mapboxKey: function() {
-      return process.env.MAPBOX_KEY;
-    },
-  },
-}));
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+    helpers: {
+      mapboxKey: function() {
+        return process.env.MAPBOX_KEY;
+      }
+    }
+  })
+);
 
-app.set('view engine', 'handlebars');
-applyEnforceHttps(app)
+app.set("view engine", "handlebars");
+applyEnforceHttps(app);
 applyMorganMiddleware(app);
 
-
-app.get('/', (request, response) => {
-  response.render('home');
+app.get("/", (request, response) => {
+  response.render("home");
 });
 
-app.get('/about', (request, response) => {
-  response.render('about');
+app.get("/about", (request, response) => {
+  response.render("about");
 });
 
-
-
-app.get('/api/sample-bins', (request, response) => {
+app.get("/api/sample-bins", (request, response) => {
   response.json(sampleData);
 });
 
+app.post("/api/bins", (request, response) => {
+  const binRequest = response.req.body;
+  Bin.create({}, {
+    type: binRequest["bin[type]"],
+    lng: binRequest["bin[lng]"],
+    lat: binRequest["bin[lat]"]
+  });
+  // bin = {
+  //   type: binRequest["bin[type]"],
+  //   lng: binRequest["bin[lng]"],
+  //   lat: binRequest["bin[lat]"]
+  // };
+  console.log("bin type", response.req.body["bin[type]"]);
+  console.log("bin lng", response.req.body["bin[lng]"]);
+  console.log("bin lat", response.req.body["bin[lat]"]);
+});
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).render('404');
+  response.status(404).render("404");
 };
 
 app.use(unknownEndpoint);
--- routes/api/bins.js
+++ routes/api/bins.js
@@ -1,22 +1,18 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
-const Bin = mongoose.model('Bin');
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const Bin = mongoose.model("Bin");
 
-
-router.get('/', (request, response) => {
-  Bin.find({},
-      {'geometry': 1,
-        'type': 1,
-        '_id': 0,
-        'properties': 1}).
-      populate('properties').
-      exec(function(err, bins) {
-        // if (err) return err
-        const binsCollection = {
-          'type': 'FeatureCollection',
-          'features': bins};
-        response.json(binsCollection);
-      });
+router.get("/", (request, response) => {
+  Bin.find({}, { geometry: 1, type: 1, _id: 0, properties: 1 })
+    .populate("properties")
+    .exec(function(err, bins) {
+      // if (err) return err
+      const binsCollection = {
+        type: "FeatureCollection",
+        features: bins
+      };
+      response.json(binsCollection);
+    });
 });
 
 module.exports = router;
