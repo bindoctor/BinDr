8a5b324afc5371cd03e08b526ab0a169caeb9fdb
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-07-15 16:15:05 +0100

    show marker on map click, marker can be dragged, show window to add bins, implement post request from adding bins

--- public/map/mapbox.js
+++ public/map/mapbox.js
@@ -13,28 +13,81 @@ const geoLocation = new mapboxgl.GeolocateControl({
   showUserLocation: true
 });
 
-map.on('load', async function () {
+map.on('load', function () {
   geoLocation.trigger();
-
-  await map.loadImage('/map-markers/paper.png', (error, image) => map.addImage('/map-markers/paper.png', image));
-  await map.loadImage('/map-markers/plastic.png', (error, image) => map.addImage('/map-markers/plastic.png', image));
-  await map.loadImage('/map-markers/mixed.png', (error, image) => map.addImage('/map-markers/mixed.png', image));
-  await map.loadImage('/map-markers/glass.png', (error, image) => map.addImage('/map-markers/glass.png', image));
-
+  map.loadImage('/map-markers/mixed.png', function (error, image) {
+    if (error) throw error;
+    map.addImage('recyclingBin', image);
+    map.addLayer({
+      "id": "points",
+      "type": "symbol",
+      "source": "allBins",
+      "layout": {
+        "icon-image": "recyclingBin",
+        "icon-size": 0.3
+      }
+    });
+  });
   map.addSource('allBins', {
     type: 'geojson',
     data: '/api/bins'
   });
-
-  map.addLayer({
-    "id": "points",
-    "type": "symbol",
-    "source": "allBins",
-    "layout": {
-      "icon-image": ['get','iconUrl', ['get', 'icon']],
-      "icon-size": 0.3
-    }
-  });
 });
 
 map.addControl(geoLocation);
+
+let addBinMarker;
+
+let lngLat;
+
+map.on('click', (e) => {
+  if(!addBinMarker) {
+    // create DOM element for the marker
+    var el = document.createElement('div');
+    el.id = 'mapboxgl-mixed-bin-marker';
+    // el.draggable = true;
+  
+    // create the marker
+    addBinMarker = new mapboxgl.Marker({draggable: true, element: el})
+        .setLngLat(e.lngLat)
+        .addTo(map);
+    var addButton = document.getElementById('add-box');
+
+    addButton.style.display = 'block';
+        
+    function onDragEnd() {
+      lngLat = addBinMarker.getLngLat();
+      console.log(lngLat)
+      }
+      
+    addBinMarker.on('dragend', onDragEnd)
+  } else {
+    addBinMarker.setLngLat(e.lngLat)
+  }
+
+});
+
+$(document).ready(function() {
+  
+  $('#submit-bin').click(function() {
+    markerLngLat = addBinMarker.getLngLat()
+    console.log(markerLngLat)
+    var binType = $("input:radio[name='bintype']:checked").attr('id');
+    console.log(binType)
+    $.ajax({
+      type: 'POST',
+      url: `/api/bins`,
+      data: {
+        bin: {
+          type: binType,
+          lng: markerLngLat.lng,
+          lat: markerLngLat.lat
+        }
+      }
+    })
+    .done(function(result){
+      console.log(result)
+      callback(result)
+    })
+  })
+})
