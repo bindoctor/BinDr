08b8f163b8eb384ff7fe2ddd815137a4c4f3c859
Anthony Hull <m@anthonyhull.me>
2019-07-15 14:33:08 +0100

    cypress runs but not in headless. https://github.com/mapbox/mapbox-gl-js/issues/8287

--- cypress/integration/visual/homePage_spec.js
+++ cypress/integration/visual/homePage_spec.js
@@ -7,8 +7,17 @@ describe('Homepage', () => {
     cy.server()
     cy.route({
       method: 'GET',      // Route all GET requests
-      url: /bins/*',
+      url: '/api/bins/*',
       response: '@binsJSON'        // and force the response to be: []
     }).as('bins')
   })
+
+  it('can recieve a non empty reply from the API', function () {
+    // this.user exists
+    cy.visit('/')
+    // cy.wait('@bins').then((xhr) => {
+    //   assert.isNotNull(xhr.response.body.data, 'list all bins API call has data')
+    // })
+  })
 });
+
